94dcb4e6552561730b11f53d592173c8c2967f28
horozco75 <anthoso3@gmail.com>
2024-09-01 23:24:51 -0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using app.Models;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Usuarios(UserLogin datos)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Index");
+            }
+
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ UserLogin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace app.Models
+{
+    public class UserLogin
+    {
+        static string cadena = "Data Source=HOROZCO;Initial Catalog=DBAPI;Integrated Security=true";
+
+
+        [Required(ErrorMessage = "<font color= 'red'>El usuario es Requerido</font>")]
+        [Display(Name = "Usuario Name")]
+        public string User { get; set; }
+
+        [Required(ErrorMessage = "<font color= 'red'>El usuario es Requerido</font>")]
+        [Display(Name = "Contraseña")]
+        public String Password { get; set; }
+        public String Nombre { get; set; }
+
+        public bool Login()
+        {
+            bool registrado;
+            string mensaje;
+
+            if (oUsuario.Clave == oUsuario.ConfirmarClave)
+            {
+
+                oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Las contraseñas no coinciden";
+                return View();
+            }
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_validarUsuario", cn);
+                cmd.Parameters.AddWithValue("Correo", oUsuario.Correo);
+                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+                cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+            }
+
+        }
+
+
+        public static string ConvertirSha256(string texto)
+        {
+            //using System.Text;
+            //USAR LA REFERENCIA DE "System.Security.Cryptography"
+
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+    }
+}
